77253060b8911a98ca06246cd6baf3200a23649a
soumasish-das <45025265+soumasish-das@users.noreply.github.com>
2021-07-30 18:34:49 +0530

    Update spark-excel_example.py

--- spark-excel_example.py
+++ spark-excel_example.py
@@ -1,56 +1,58 @@
-#Read and write with Spark dataframe directly from/to excel file using spark-excel library
+# Read and write with Spark dataframe directly from/to excel file using spark-excel library
 
-#----------------------------
+# ----------------------------
 # spark-excel library source:
-#----------------------------
+# ----------------------------
 # https://github.com/crealytics/spark-excel
 
-#--------------------------------------
+# --------------------------------------
 # spark-excel version and dependencies:
-#--------------------------------------
+# --------------------------------------
 # Maven Repository:
 # https://mvnrepository.com/artifact/com.crealytics/spark-excel_2.12/0.13.7
 
 # spark-excel information from the Maven repository page:
-#--------------------------------------------------------
+# --------------------------------------------------------
 # groupId: com.crealytics
 # artifactId: spark-excel_2.12
 # version: 0.13.7
 
 # Format: spark.jars.packages  groupId:artifactId:version
 
-#---------------------------
+# ---------------------------
 # INSTALLATION INSTRUCTIONS:
-#---------------------------
+# ---------------------------
 # 1. Open "spark-defaults.conf" in "%SPARK_HOME%\conf" folder
 # 2. Add "spark.jars.packages  com.crealytics:spark-excel_2.12:0.13.7"
 
-#--------------------------------------------------------
+# --------------------------------------------------------
 # Reference for read/write excel from/to spark dataframe:
-#--------------------------------------------------------
+# --------------------------------------------------------
 # https://github.com/crealytics/spark-excel/blob/main/README.md
 
 
 import findspark
+
 findspark.init()
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("ReadExcel").getOrCreate()
+spark = SparkSession.builder.appName("ReadExcel") \
+    .config("spark.jars.packages", "com.crealytics:spark-excel_2.12:0.13.7") \
+    .getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
 
-#-----------------------------------------
+# -----------------------------------------
 # Get Spark UI page URL from Spark Context
-#-----------------------------------------
-#from pyspark import SparkContext
+# -----------------------------------------
+# sc = spark.sparkContext
+# print(sc.uiWebUrl)
+# -----------------------------------------
 
-#sc = SparkContext.getOrCreate()
-#print(sc.uiWebUrl)
-#-----------------------------------------
-
-#Read from excel file
+# Read from excel file
 sparkDF = spark.read.format("com.crealytics.spark.excel") \
-                    .options(header='true', inferSchema='true', dataAddress="'Data'!A1") \
-                    .load("C:\\Users\\Vicky\\Minnie\\SampleXLSFile.xls")
+    .options(header='true', inferSchema='true', dataAddress="'Data'!A1") \
+    .load("C:\\Users\\Vicky\\Minnie\\SampleXLSFile.xls")
 
 print("Data read from excel file successfully.\n")
 
@@ -62,11 +64,11 @@ spark.sql("select count(*) from excel_data").show()
 excelWriteDF = spark.sql("select * from excel_data where UserId < 10000")
 print("excelWriteDF row count: " + str(excelWriteDF.count()))
 
-#Write into excel file
+# Write into excel file
 excelWriteDF.write.format("com.crealytics.spark.excel") \
-                  .options(header='true', dataAddress="'Result'!A1") \
-                  .mode("overwrite") \
-                  .save("C:\\Users\\Vicky\\Minnie\\spark-excel-write-test.xlsx")
+    .options(header='true', dataAddress="'Result'!A1") \
+    .mode("overwrite") \
+    .save("C:\\Users\\Vicky\\Minnie\\spark-excel-write-test.xlsx")
 
 print("\nData written to excel file successfully.")
 
