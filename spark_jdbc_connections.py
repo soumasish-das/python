5b2c2250bd4f420a8e1c2b6252e245035e22b4f7
soumasish-das <45025265+soumasish-das@users.noreply.github.com>
2021-11-10 15:10:21 +0530

    Create spark_jdbc_connections.py

---
+++ spark_jdbc_connections.py
@@ -0,0 +1,47 @@
+# Spark JDBC connection definitions
+
+# Generic function to set connection options (like url, user, password etc.) and get data
+def getData(spark_df, connection_dict):
+    for key in connection_dict:
+        spark_df = spark_df.option(key, connection_dict[key])
+    return spark_df.load()
+
+
+# Get SQLite data
+def getSqLiteDF(spark_obj, connection_dict):
+    connection_dict['url'] = "jdbc:sqlite://" + connection_dict['url']
+    sqLiteDF = spark_obj.read.format("jdbc").option("driver", "org.sqlite.JDBC")
+    sqLiteDF = getData(sqLiteDF, connection_dict)
+    return sqLiteDF
+
+
+# Get PostgreSQL/GreenPlum data
+def getPgGpDF(spark_obj, connection_dict):
+    connection_dict['url'] = "jdbc:postgresql://" + connection_dict['url']
+    pgGpDF = spark_obj.read.format("jdbc").option("driver", "org.postgresql.Driver")
+    pgGpDF = getData(pgGpDF, connection_dict)
+    return pgGpDF
+
+
+# Get Oracle data
+def getOracleDF(spark_obj, connection_dict):
+    connection_dict['url'] = "jdbc:oracle:thin:@" + connection_dict['url']
+    oracleDF = spark_obj.read.format("jdbc").option("driver", "oracle.jdbc.driver.OracleDriver")
+    oracleDF = getData(oracleDF, connection_dict)
+    return oracleDF
+
+
+# Get SQL Server data
+def getSqlServerDF(spark_obj, connection_dict):
+    connection_dict['url'] = "jdbc:sqlserver://" + connection_dict['url']
+    oracleDF = spark_obj.read.format("jdbc").option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+    oracleDF = getData(oracleDF, connection_dict)
+    return oracleDF
+
+
+# Get MySQL data
+def getMySqlDF(spark_obj, connection_dict):
+    connection_dict['url'] = "jdbc:mysql://" + connection_dict['url']
+    oracleDF = spark_obj.read.format("jdbc").option("driver", "com.mysql.cj.jdbc.Driver")
+    oracleDF = getData(oracleDF, connection_dict)
+    return oracleDF
