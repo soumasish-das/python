8fe1ef1261925921f11769fb11b2acf1e563b6c6
soumasish-das <45025265+soumasish-das@users.noreply.github.com>
2021-11-10 15:14:13 +0530

    Update spark_jdbc_connections.py

--- spark_jdbc_connections.py
+++ spark_jdbc_connections.py
@@ -11,37 +11,32 @@ def getData(spark_df, connection_dict):
 def getSqLiteDF(spark_obj, connection_dict):
     connection_dict['url'] = "jdbc:sqlite://" + connection_dict['url']
     sqLiteDF = spark_obj.read.format("jdbc").option("driver", "org.sqlite.JDBC")
-    sqLiteDF = getData(sqLiteDF, connection_dict)
-    return sqLiteDF
+    return getData(sqLiteDF, connection_dict)
 
 
 # Get PostgreSQL/GreenPlum data
 def getPgGpDF(spark_obj, connection_dict):
     connection_dict['url'] = "jdbc:postgresql://" + connection_dict['url']
     pgGpDF = spark_obj.read.format("jdbc").option("driver", "org.postgresql.Driver")
-    pgGpDF = getData(pgGpDF, connection_dict)
-    return pgGpDF
+    return getData(pgGpDF, connection_dict)
 
 
 # Get Oracle data
 def getOracleDF(spark_obj, connection_dict):
     connection_dict['url'] = "jdbc:oracle:thin:@" + connection_dict['url']
     oracleDF = spark_obj.read.format("jdbc").option("driver", "oracle.jdbc.driver.OracleDriver")
-    oracleDF = getData(oracleDF, connection_dict)
-    return oracleDF
+    return getData(oracleDF, connection_dict)
 
 
 # Get SQL Server data
 def getSqlServerDF(spark_obj, connection_dict):
     connection_dict['url'] = "jdbc:sqlserver://" + connection_dict['url']
     sqlServerDF = spark_obj.read.format("jdbc").option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
-    sqlServerDF = getData(sqlServerDF, connection_dict)
-    return sqlServerDF
+    return getData(sqlServerDF, connection_dict)
 
 
 # Get MySQL data
 def getMySqlDF(spark_obj, connection_dict):
     connection_dict['url'] = "jdbc:mysql://" + connection_dict['url']
     mySqlDF = spark_obj.read.format("jdbc").option("driver", "com.mysql.cj.jdbc.Driver")
-    mySqlDF = getData(mySqlDF, connection_dict)
-    return mySqlDF
+    return getData(mySqlDF, connection_dict)
