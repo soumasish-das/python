eb0c64f4ee9077d41bee24f4ff947175198188e2
soumasish-das <45025265+soumasish-das@users.noreply.github.com>
2022-05-26 18:31:14 +0530

    Update resource_tracker.py

--- resource_tracker.py
+++ resource_tracker.py
@@ -1,14 +1,30 @@
+import argparse
 import pandas as pd
-# import os
-
 import sendmail
 
-# resource_tracker = "C:\\Users\\Vicky\\Minnie\\Resource_tracker.xlsx"
-resource_tracker = input("Enter the path of the resource tracker excel file: ")
-data = pd.read_excel(resource_tracker)
+parser = argparse.ArgumentParser(description='Resource Tracker Automated Email: '
+                                             'Script to generate and send email for resource tracking.')
 
-# Replace all NaN with blanks
-data = data.fillna('')
+# Required arguments
+required_args = parser.add_argument_group('required arguments')
+required_args.add_argument("-l", "--excel-location", help="Location of the Resource Tracker excel file", required=True)
+required_args.add_argument("-o", "--output-dir", help="Output directory to generate HTML file for email", required=True)
+required_args.add_argument("-tn", "--to-list-names", help="To-list names to be used in the email body", required=True)
+required_args.add_argument("-sn", "--sender-name", help="Sender's name to be used in the email body", required=True)
+required_args.add_argument("-s", "--subject", help="Subject of the email", required=True)
+required_args.add_argument("-te", "--to-list-addresses", help="Comma separated to-list email addresses", required=True)
+required_args.add_argument("-se", "--sender-address", help="Sender's email address", required=True)
+
+# Optional arguments
+parser.add_argument("-ce", "--cc-list-addresses", help="Comma separated cc-list email addresses")
+parser.add_argument("-a", "--attachment", help="File attachment to be added to the email")
+
+args = parser.parse_args()
+
+data = pd.read_excel(args.excel_location)
+
+# Replace all NaN with '-'
+data = data.fillna('-')
 
 # Set CSS style
 html = '<html>\n<head>\n<style>\n'
@@ -21,7 +37,7 @@ html += '</style>\n</head>\n'
 # Begin HTML and email body
 html += '<body style="font-size: 11pt">\n'
 html += '<p>\n'
-html += 'Hi,<br><br>\n'
+html += 'Hi {},<br><br>\n'.format(args.to_list_names)
 html += 'Please see the EOD task status of each resource below:<br>\n</p>\n'
 html += '<table>\n'
 
@@ -41,20 +57,19 @@ for i in range(len(data)):
 html += '</table>\n'
 
 # End of mail body and HTML
-html += '<p>\nBest regards,<br>Shruti Jayaswal\n</p>\n'
+html += '<p>\nBest regards,<br>{}\n</p>\n'.format(args.sender_name)
 html += '</body>\n</html>'
 
+mail_file = args.output_dir + "\\Mail.html"
+
 # Write HTML contents to file
-with open("Mail.html", "w") as file:
+with open(mail_file, "w") as file:
     file.write(html)
 
-# Open the HTML file in the default browser
-# os.startfile("Mail.html")
-
-sendmail.email(to_list='soumasishdas@gmail.com,jayaswal.shruti@gmail.com',
-               sender='soumasishdas@gmail.com',
-               subject='Resource Tracker Test automated email',
-               cc_list='soumasishdas@gmail.com,jayaswal.shruti@gmail.com',
-               mail_html_file='Mail.html',
-               attachment=resource_tracker
+sendmail.email(to_list=args.to_list_addresses,
+               sender=args.sender_address,
+               subject=args.subject,
+               cc_list=args.cc_list_addresses,
+               mail_html_file=mail_file,
+               attachment=args.attachment
                )
